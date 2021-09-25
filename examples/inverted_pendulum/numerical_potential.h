a20734394844872a53753b77bbcffc46fe6389f6
Plushka21 <54961672+Plushka21@users.noreply.github.com>
2021-09-25 21:41:55 +0300

    C headers for inverted pendulum

---
+++ examples/inverted_pendulum/numerical_potential.h
@@ -0,0 +1,5 @@
+#include <math.h>
+void numerical_potential(double g, double l, double m, double theta, double *out_1096773990258309843) {
+   out_1096773990258309843[0] = 0;
+   out_1096773990258309843[1] = -g*l*m*sin(theta);
+}
