85121c1caa9bd2dea9c6d8ee4f1fb46e93c14b21
Plushka21 <albugg2001@gmail.com>
2021-09-25 11:21:18 +0300

    Added docsting and dependence order

--- lib/symbolical_dynamics/misc/_diff.py
+++ lib/symbolical_dynamics/misc/_diff.py
@@ -1,60 +1,89 @@
 from sympy import Function, Derivative, Matrix, symbols, diff
 
-# TODO: 
-#  Add dependence_order option: dependence of expression on time derevitives of variable
-#  Write documantation strings 
 
-def difftotal(expr, diffby, diffmap):
-    # Replace all symbols in the diffmap by a functional form
-    fnexpr = expr.subs({s:Function(str(s))(diffby) for s in diffmap})
-    diffexpr = diff(fnexpr, diffby)
+def diff_total(expr, diff_by, diff_map):
+    """
+    Replace variables with their differentials in expression
+    @param expression expr: expression to differentiate
+    @param symbol diff_by: variable to differentiate with respect to
+    @param dictionary diff_map: map for n-th order derivative
+    @return expression: result of differentiation
+    """
+    # Replace all symbols in the diff_map by a functional form
+    fun_expr = expr.subs({s: Function(str(s))(diff_by) for s in diff_map})
+    diff_expr = diff(fun_expr, diff_by)
 
-    # Replace the Derivatives with the variables in diffmap
-    derivmap = {Derivative(Function(str(v))(diffby), diffby):dv 
-                for v, dv in diffmap.items()}
-   
-    finaldiff = diffexpr.subs(derivmap)
+    # Create map to replace each 'Derivative' with the variable
+    deriv_map = {Derivative(Function(str(v))(diff_by), diff_by): dv
+                 for v, dv in diff_map.items()}
+    # Replace the 'Derivatives' with the variables in diff_map
+    final_diff = diff_expr.subs(deriv_map)
 
     # Replace functional form with their symbols
-    output = finaldiff.subs({Function(str(s))(diffby):s for s in diffmap})
+    result = final_diff.subs({Function(str(s))(diff_by): s for s in diff_map})
 
-    return output
+    return result
 
 
-# Transform  vector of strings variables to their derevvitives in LaTeX form 
-def diff_symbols(variable, order = 1, texform = False):
-    n = len(variable)
-    dvariable = [0]*n
-    if order>0:
-      for i in range(n):
-        if texform:
-          dvariable[i] = '\d' + 'd'*(order-1) + 'ot{' + str(variable[i]) +'}'
-        else:
-          dvariable[i] = 'd'*(order) + str(variable[i])
-
-      out = Matrix(symbols(dvariable))
+def diff_symbols(variables, diff_order=1, text_form=False):
+    """
+    Transform  vector of strings variables to their derivatives in LaTeX form.
+    @param array.pyi variables: variables to differentiate
+    @param int diff_order: order of derivative of expression
+    @param bool text_form: return expression in text form or not
+    @return matrix: matrix with variables or their corresponding differential form
+    """
+    n = len(variables)
+    diff_variable = [0] * n
+    if diff_order > 0:
+        for i in range(n):
+            # For each varibale find its derivative
+            if text_form:
+                diff_variable[i] = '\d' + 'd' * (diff_order - 1) + 'ot{' + str(variables[i]) + '}'
+            else:
+                diff_variable[i] = 'd' * diff_order + str(variables[i])
+        # Matrix with differentials of variables
+        var_matrix = Matrix(symbols(diff_variable))
     else:
-      out = Matrix(variable)
-    return out
+        # Matrix only with variables
+        var_matrix = Matrix(variables)
+    return var_matrix
 
 
-# build differential map for n-th order derivetive
-def build_diffmap(variable, order=1, texform = False):
+def build_diffmap(variables, diff_order=1, text_form=False):
+    """
+    Build differential map for n-th order derivative.
+    @param array.pyi variables: variables to differentiate
+    @param int diff_order: order of derivative of expression
+    @param bool text_form: return expression in text form or not
+    @return dictionary: map for n-th order derivative
+    """
     diff_map = dict()
-    n = len(variable)
-    for i in range(order+1):
-      di_x = diff_symbols(variable, i-1, texform = texform)
-      ddi_x = diff_symbols(variable, i, texform = texform)
-      for j in range(n):
-        diff_map[di_x[j]] = ddi_x[j]
+    for i in range(diff_order + 1):
+        # For each variable find corresponding differential form
+        di_x = diff_symbols(variables, diff_order=i - 1, text_form=text_form)
+        ddi_x = diff_symbols(variables, diff_order=i, text_form=text_form)
+        for j in range(len(variables)):
+            # Put these forms in the dictionary
+            diff_map[di_x[j]] = ddi_x[j]
     return diff_map
 
 
-def time_diff(expression, variables, order = 1, dorder = 1, texform = False):
+def time_diff(expression, variables, order=1, diff_order=1, text_form=False):
+    """
+    Find and return the derivative of expression with respect to t.
+    @param expression: expression that we want to differentiate
+    @param array.pyi variables: variables to differentiate
+    @param int order: oder of derivative we need
+    @param int diff_order: order of derivative of expression itself
+    @param bool text_form: return expression in text form or not
+    @return expression: derivative of expression with respect to t for given variables
+    """
     t = symbols('t')
-    diffmap = build_diffmap(variables, dorder, texform = texform)
-    result = difftotal(expression, t, diffmap)
-    # for i in range(order):
-    #   diffmap = build_diffmap(variables, dorder)
-    #   result = difftotal(result, t, diffmap)
+    result = expression
+    for i in range(order):
+        # Find differential map
+        diff_map = build_diffmap(variables, diff_order, text_form=text_form)
+        # Get result of differentiation
+        result = diff_total(result, t, diff_map)
     return result
